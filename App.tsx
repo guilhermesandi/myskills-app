be4b2e65f8ca063c8319ada6abe23b8598b8a997
Guilherme Sandi <guilherme_sandi@hotmail.com>
2023-01-25 19:53:14 -0300

    Install Detox

--- App.tsx
+++ App.tsx
@@ -1,13 +1,12 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
-import { Home } from './src/pages/Home';
+import {StatusBar} from 'react-native';
+import {Home} from './src/pages/Home';
 
-
-export default function App(){
+export default function App() {
   return (
     <>
       <StatusBar barStyle="light-content" />
-      <Home/>
+      <Home />
     </>
-  )
+  );
 }
